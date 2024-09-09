73c0d91749f42144cafbed998c0f354ad274193b
WhiteCow OwO <94701510+whitecow410@users.noreply.github.com>
2024-09-09 16:14:42 +0800

    Update URC_2024.py

--- URC_2024.py
+++ URC_2024.py
@@ -136,7 +136,7 @@ def fix(event=stop):
         elif ir_right.is_black() and not ir_left.is_black():
             turn_left()
         else:
-            break
+            break  
     event()
 
 
@@ -164,6 +164,44 @@ def square():
 
 
 def setup(threshold='auto'):
+    temp = []
+    if not threshold:
+        for i in range(2):
+            while button_a.is_pressed():
+                pass
+            display.show("W" if i == 0 else "B", delay=0)
+            while not button_a.is_pressed():
+
+                pass
+            temp += [ir_left.get_value(), ir_right.get_value()]
+        while button_a.is_pressed():
+            pass
+        ir_left.set_threshold(int((temp[0] + temp[2]) / 2))
+        ir_right.set_threshold(int((temp[1] + temp[3]) / 2))
+    elif threshold == 'auto':
+        display.show("Auto", delay=0)
+        while button_a.is_pressed():
+            pass
+        while not button_a.is_pressed():
+            pass
+        # if not ir_left.get_value() >= ir_left.threshold or not ir_right.get_value() >= ir_right.threshold:
+        #     move_forward()
+        #     while not ir_left.get_value() >= ir_left.threshold or not ir_right.get_value() >= ir_right.threshold:
+        #         pass
+        #     stop()
+        temp += [ir_left.get_value(), ir_right.get_value()]
+        for index, ir in enumerate([ir_left, ir_right]):
+            if temp[index] - ir.get_value() <= 1000:
+                move_forward()
+                while temp[index] - ir.get_value() <= 1000:
+                    pass
+            stop()
+            temp += [ir.get_value()]
+        ir_left.set_threshold(int((temp[0] + temp[2]) / 2))
+        ir_right.set_threshold(int((temp[1] + temp[3]) / 2))
+    elif isinstance(threshold, list):
+        ir_left.set_threshold(threshold[0])
+        ir_right.set_threshold(threshold[1])
     display.show("G", delay=0)
     while not button_a.is_pressed():
         pass
@@ -175,24 +213,7 @@ def setup(threshold='auto'):
 setup()
 # Part 1
 pick(55)
-temp = []
-if threshold == 'auto':
-    display.show("Auto", delay=0)
-    temp += [ir_left.get_value(), ir_right.get_value()]
-    for index, ir in enumerate([ir_left, ir_right]):
-        if temp[index] - ir.get_value() <= 1000:
-            move_forward()
-            while temp[index] - ir.get_value() <= 1000:
-                pass
-        stop()
-        temp += [ir.get_value()]
-    ir_left.set_threshold(int((temp[0] + temp[2]) / 2))
-    ir_right.set_threshold(int((temp[1] + temp[3]) / 2))
-    display.clear()
-else:
-    forward_line(move_forward, 1)
-
-forward_line(move_forward, 2)
+forward_line(move_forward, 3)
 move_forward()
 time.sleep(0.5)
 stop()
@@ -201,16 +222,16 @@ pick(100, 0.05)
 time.sleep(0.5)
 forward_line(move_backward, 2)
 move_backward()
-time.sleep(0.53)
+time.sleep(0.52)
 stop()
 drop()
 
 # Part 2
 time.sleep(0.5)
 set_power(150)
-forward_line(move_backward)
+forward_line(move_backward) 
 move_backward()
-time.sleep(1.3)
+time.sleep(1.5)
 turn_right()
 time.sleep(0.6)
 move_forward()
@@ -228,7 +249,7 @@ while ir_center.get_value() >= 380:
     move_backward()
     fix()
 time.sleep(0.5)
-pick(115, 0.05)
+pick(110, 0.05)
 set_power(80)
 time.sleep(0.5)
 set_hight(135)
@@ -237,23 +258,29 @@ time.sleep(0.5)
 
 # move_forward()
 # time.sleep(0.3)
-while ir_center.get_value() <= 380:
+while ir_center.get_value() <= 300:
     move_forward()
     fix()
 time.sleep(0.5)
 
 # >>>
-set_hight(125)
+turn_left()
+time.sleep(0.07)
+stop()
 time.sleep(0.5)
-pick(100, 0.05)
+
+set_hight(115)
+time.sleep(0.5)
+pick(95)
 time.sleep(0.5)
 drop()
+time.sleep(0.5)
 set_hight(0)
 time.sleep(0.5)
 forward_line(move_forward, 1)
 move_forward()
 time.sleep(0.3)
-while ir_center.get_value() >= 380:
+while ir_center.get_value() >= 390:
     move_backward()
     fix()
 time.sleep(0.5)
@@ -261,18 +288,28 @@ time.sleep(0.5)
 pick(100, 0.05)
 set_power(80)
 time.sleep(0.5)
-set_hight(85)
+set_hight(135) ### 85
 time.sleep(0.5)
 while ir_center.get_value() <= 340:
     move_forward()
     fix()
 time.sleep(0.5)
-set_hight(77)
+set_hight(115) ###76
+time.sleep(0.5) 
+drop()
 time.sleep(0.5)
-pick(100)
+
+turn_left()
+time.sleep(0.07)
+stop()
+time.sleep(0.5)
+set_hight(40)
+time.sleep(0.5)
+pick(100, 0.01)
 time.sleep(0.5)
 drop()
 time.sleep(0.5)
+
 set_hight(0)
 set_power(75)
 time.sleep(0.5)
@@ -294,11 +331,16 @@ time.sleep(0.5)
 move_backward()
 time.sleep(1)
 turn_left()
-time.sleep(1.5)
+
+#time.sleep(1.59)
+auto_left()
+turn_left()
+time.sleep(0.75)
+
 stop()
 time.sleep(0.5)
 pick(0)
-for i in range(9):
+for i in range(8):
     move_forward()
     time.sleep(0.1)
 stop()
@@ -338,7 +380,7 @@ while ir_center.get_value() >= 380:
     move_backward()
     fix()
 time.sleep(0.5)
-pick(115, 0.05)
+pick(110 , 0.05)
 set_power(80)
 time.sleep(0.5)
 set_hight(135)
@@ -347,41 +389,59 @@ time.sleep(0.5)
 
 # move_forward()
 # time.sleep(0.3)
-while ir_center.get_value() <= 380:
+while ir_center.get_value() <= 300:
     move_forward()
     fix()
 time.sleep(0.5)
 
 # >>>
-set_hight(125)
+turn_left()
+time.sleep(0.07)
+stop()
 time.sleep(0.5)
-pick(100, 0.05)
+
+set_hight(115)
+time.sleep(0.5)
+pick(95)
 time.sleep(0.5)
 drop()
-set_hight(0)
-time.sleep()
+time.sleep(0.5)
+set_hight(0) 
+time.sleep(0.5)
 forward_line(move_forward, 1)
 move_forward()
 time.sleep(0.3)
-while ir_center.get_value() >= 380:
+while ir_center.get_value() >= 390:
     move_backward()
     fix()
 time.sleep(0.5)
 
+
 pick(100, 0.05)
 time.sleep(0.5)
-set_hight(95)
+set_hight(135) ### 95
 time.sleep(0.5)
 while ir_center.get_value() <= 340:
     move_forward()
     fix()
 time.sleep(0.5)
-set_hight(77)
+set_hight(115) ### 77
+time.sleep(0.5) 
+drop()
 time.sleep(0.5)
-pick(100)
+ 
+turn_left()
+time.sleep(0.07)
+stop()
+time.sleep(0.5)
+set_hight(40)
+time.sleep(0.5)
+pick(100, 0.01)
 time.sleep(0.5)
 drop()
 time.sleep(0.5)
+
+
 set_hight(0)
 time.sleep(0.5)
 forward_line(move_forward)
@@ -389,7 +449,7 @@ time.sleep(0.5)
 move_backward()
 time.sleep(0.35)
 stop()
-time.sleep(0.5)
+time.sleep(0.5)  
 pick(100, 0.05)
 time.sleep(0.3)
 move_backward()
@@ -400,7 +460,7 @@ time.sleep(0.5)
 set_hight(135)
 time.sleep(0.5)
 turn_right()
-time.sleep(0.80)
+time.sleep(1.3)
 stop()
 time.sleep(0.5)
 for i in range(11):
@@ -416,4 +476,4 @@ set_hight(140)
 time.sleep(0.5)
 move_backward()
 time.sleep(0.8)
-stop()
+stop()  
